0165f17e75f5b4894e0f73414b2958fb807361e6
shunxu huang <shuang@gener8.net>
2018-08-30 16:13:10 -0700

    initial commit

---
+++ AwesomeLib.Tests/AwesomeFunctionCollectionTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit; 
+
+namespace AwesomeLib.Tests
+{
+    public class AwesomeFunctionCollectionTests
+    {
+        [Fact]
+        public void GetAwesomeStringTest()
+        {
+            Assert.True(AwesomeFunctionCollection.AwesomeString == AwesomeFunctionCollection.GetAwesomeString()); 
+        }
+    }
+}
---
+++ AwesomeLib/AwesomeFunctionCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeLib
+{
+    public static class AwesomeFunctionCollection
+    {
+        public static string AwesomeString = "Awesome"; 
+        public static string GetAwesomeString()
+        {
+            return AwesomeString; 
+        }
+    }
+}
