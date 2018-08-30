b2e0bf1cb585e9928920db8bfc60b01a32251292
shunxu huang <shuang@gener8.net>
2018-08-30 16:29:20 -0700

    Added more example

--- AwesomeLib.Tests/AwesomeFunctionCollectionTests.cs
+++ AwesomeLib.Tests/AwesomeFunctionCollectionTests.cs
@@ -9,5 +9,26 @@ namespace AwesomeLib.Tests
         {
             Assert.True(AwesomeFunctionCollection.AwesomeString == AwesomeFunctionCollection.GetAwesomeString()); 
         }
+
+        // replace for SetUp in NUnit
+        public AwesomeFunctionCollectionTests()
+        {
+            // you set up your resources here 
+        }
+
+        [Fact] // has to be marked fact
+        public void Dispose() // this is equivalent to TearDown in NUnit
+        {
+            // do your clean up here 
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(3, 3)]
+        [InlineData(2, 1)] // this case will fail
+        public void InlineArgumentExample(int i1, int i2)
+        {
+            Assert.True(i1 == i2); 
+        }
     }
 }
